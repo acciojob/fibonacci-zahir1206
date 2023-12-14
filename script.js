be6d01de7e53191cb0a91ed6362615f416f89a00
Md Zahirul Haque <116150226+zahir1206@users.noreply.github.com>
2023-12-14 21:58:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 function fibonacci(num) {
 // your code here
-	 if (num <= 1) {
+    if (num <= 1) {
         return num;
-    } else {
-        return fibonacci(num - 1) + fibonacci(num - 2);
     }
+
+    let fibPrev = 0;
+    let fibCurrent = 1;
+
+    for (let i = 2; i <= num; i++) {
+        let temp = fibPrev + fibCurrent;
+        fibPrev = fibCurrent;
+        fibCurrent = temp;
+    }
+
+    return fibCurrent;
 }
 
 module.exports = fibonacci;
